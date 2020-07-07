7de3e0647320bfbdb6bb1c0ec068c979ad6af062
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2020-07-07 00:08:47 -0300

    app updated

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,173 +1,162 @@
-function dateChecker(possibleDate) {
-    var datePattern = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
-
-    var res = possibleDate.match(datePattern);
-    return (res !== null)
-}
-
-const getTripData = async(place, date) => {
+const getTravelData = async (place, date) => {
     try {
-        const resp = await fetch(`http://localhost:8080/forcast?place=${place}&date=${date}`);
+        const resp = await fetch(`/weather?place=${place}&date=${date}`);
         const respData = await resp.json();
         return respData;
-
     } catch (error) {
-        //TODO: propper error handler
-        alert('Erorr getting card DATA!\n', error)
+        alert("Error getting form data ", error);
     }
-}
+};
 
-async function addTrip(event) {
+async function setTravel(event) {
     event.preventDefault();
-
-    const place = document.getElementById('place');
-    const date = document.getElementById('date');
-
-    if (Client.dateChecker(date.value) != true) {
-        alert('Invalid date for the trip!\nThe date should be in the following format: yyyy-MM-dd');
-        return false;
-    }
-
-    const tripData = await getTripData(place.value, date.value);
-    await saveTripsToCache(tripData);
-    displayOneTrip(tripData, getTripData.length);
+    const place = document.getElementById("place");
+    const date = document.getElementById("date");
+    const travelData = await getTravelData(place.value, date.value);
+    await saveTravels(travelData);
+    displayTravel(travelData, getTravelData.length);
     place.value = "";
     date.value = "";
 }
 
+const displayTrips = async () => {
+    //get travels
+    var travels = await getLocalTravels();
 
-const displayTrips = async() => {
-    //get trips
-    var trips = await getTripsFromCache();
+    if (!travels) return;
 
-    if (!trips) return;
+    //if travels then clear ui elements
+    const travelUI = document.getElementById("results");
+    travelUI.innerHTML = "";
 
-    //if trips then clear ui elements
-    const tripsUI = document.getElementById('results');
-    tripsUI.innerHTML = "";
+    //iterate travels and add travel to ui
+    travels.map((travel, index) => {
+        displayTravel(travel, index, travelUI);
+    });
+};
 
-    //iterate trips and add trip to ui
-    trips.map((trip, index) => {
-        displayOneTrip(trip, index, tripsUI);
-    })
-}
+document.addEventListener("DOMContentLoaded", () => displayTrips(), false);
 
-document.addEventListener('DOMContentLoaded', () => displayTrips(), false);
+const saveTravels = async (travelData) => {
+    var travels = JSON.parse(localStorage.getItem("travels"));
+    if (!travels) {
+        travels = new Array();
+    }
+    if (travels.length > 0) {
+        travelData.id = Math.max(...travels.map((x) => x.id)) + 1;
+    } else {
+        travelData.id = 1;
+    }
+    console.dir(travelData);
+    travels.push(travelData);
+    //save travels to localstorage
+    localStorage.setItem("travels", JSON.stringify(travels));
+};
 
+const getLocalTravels = async () => {
+    var travels = JSON.parse(localStorage.getItem("travels"));
+    if (!travels) return null;
+    return travels;
+};
 
-const saveTripsToCache = async(tripData) => {
-    var trips = JSON.parse(localStorage.getItem('trips'));
-    if (!trips)
-        trips = new Array();
-    // add trip to trips
-    if (trips.length > 0)
-        tripData.id = Math.max(...trips.map(x => x.id)) + 1; // trips.length;
-    else
-        tripData.id = 1;
-    console.dir(tripData)
-    trips.push(tripData);
-    //save trips to localstorage
-    localStorage.setItem('trips', JSON.stringify(trips));
-}
+const deleteLocalTravels = async (id) => {
+    let travels = await getLocalTravels();
+    if (!travels) return null;
 
+    //old don't use travels.splice(index, 1);
+    travels = travels.filter((x) => x.id != id);
+    localStorage.setItem("travels", JSON.stringify(travels));
+    await noneTravels();
+};
 
-const getTripsFromCache = async() => {
-    var trips = JSON.parse(localStorage.getItem('trips'));
-    if (!trips) return null;
-    return trips;
-}
-
-
-const delTripFromCache = async(id) => {
-    let trips = await getTripsFromCache();
-    if (!trips) return null;
-
-    //old don't use trips.splice(index, 1);
-    trips = trips.filter(x => x.id != id);
-    localStorage.setItem('trips', JSON.stringify(trips));
-    await displayNoTrips();
-}
-
-async function displayOneTrip(trip, index, tripsUI) {
-    //show no trips on screen
-    await displayNoTrips()
-    let scrollLast = false;
-    if (!tripsUI) {
-        tripsUI = document.getElementById('results');
-        scrollLast = true;
+async function displayTravel(travel, index, travelUI) {
+    //show no travels on screen
+    await noneTravels();
+    let scrollSmooth = false;
+    if (!travelUI) {
+        travelUI = document.getElementById("results");
+        scrollSmooth = true;
     }
 
-    const txt = `Trip to ${trip.location.name}, ${trip.location.countryCode} on ${trip.date} `;
-    let mainDiv = document.createElement('div');
+    const txt = `Travel to the beautiful city of ${travel.location.name}, ${travel.location.countryCode} on ${travel.date} `;
+    let mainDiv = document.createElement("div");
     mainDiv.className = "card";
-    //delete icon
-    const del = document.createElement('a');
+    const del = document.createElement("a");
     del.innerText = "X";
     del.href = "#";
-    del.className = "close"
-    del.addEventListener('click', async(ev) => {
-        console.log(`curren truip id: ${trip.id}`)
-        if (confirm(`delete trip to: ${trip.location.name}? `)) {
-            console.log('deleting index:', trip.id);
-            await delTripFromCache(trip.id);
+    del.className = "closeBtn";
+    del.addEventListener("click", async (ev) => {
+        if (
+            confirm(
+                `You want to delete your entry to: ${travel.location.name}, ${travel.location.countryCode} on ${travel.date} ? `
+            )
+        ) {
+            await deleteLocalTravels(travel.id);
             await displayTrips();
         }
     });
     mainDiv.appendChild(del);
 
     //weather image
-    if (trip.weather.icon) {
-        let wDiv = document.createElement('div');
-        wDiv.className = "weather"
-        let img = document.createElement('img');
-        img.src = `icons/${trip.weather.icon}.png`;
-        // img.style.height = "80px";
-        wDiv.appendChild(img);
-        let span = document.createElement('div');
-        span.innerHTML = `${trip.weather.temperature}&deg; with ${trip.weather.description}`;
+    if (travel.weather.icon) {
+        let wDiv = document.createElement("div");
+        wDiv.className = "weather";
+        let span = document.createElement("div");
+        span.innerHTML = `Weather: ${travel.weather.temperature}&deg; with ${travel.weather.description}`;
         wDiv.appendChild(span);
         mainDiv.appendChild(wDiv);
     } else {
-        let missingDiv = document.createElement('div');
-        missingDiv.innerHTML = `No weather available!`;
-        missingDiv.className = "missing";
-        mainDiv.appendChild(missingDiv);
+        let missingWeather = document.createElement("div");
+        missingWeather.innerHTML = `Sorry our api is kinda dumb! No weather available!`;
+        missingWeather.className = "weather";
+        mainDiv.appendChild(missingWeather);
     }
-    let picDiv = document.createElement('div');
     let img = null;
-    if (trip.image && trip.image.webformatURL) {
-        let imgDiv = document.createElement('div');
-        let img = document.createElement('img');
-        //console.dir(trip.image)
-        img.src = trip.image.webformatURL;
+    if (travel.image && travel.image.webformatURL) {
+        let imgDiv = document.createElement("div");
+        let img = document.createElement("img");
+        img.src = travel.image.webformatURL;
         imgDiv.className = "image";
         imgDiv.appendChild(img);
         mainDiv.appendChild(imgDiv);
 
-        if (scrollLast == true) {
-            img.addEventListener('load', () => {
-                img.scrollIntoView({ behavior: 'smooth' });
-            })
+        if (scrollSmooth == true) {
+            img.addEventListener("load", () => {
+                img.scrollIntoView({ behavior: "smooth" });
+            });
+        }
+    } else {
+        let imgDiv = document.createElement("div");
+        let img = document.createElement("img");
+        img.src =
+            "https://thumbs.dreamstime.com/b/no-image-available-icon-photo-camera-flat-vector-illustration-132483141.jpg";
+        img.className =
+            "noPhoto";
+        imgDiv.className = "image";
+        imgDiv.appendChild(img);
+        mainDiv.appendChild(imgDiv);
+
+        if (scrollSmooth == true) {
+            img.addEventListener("load", () => {
+                img.scrollIntoView({ behavior: "smooth" });
+            });
         }
     }
 
-    let p = document.createElement('p');
+    let p = document.createElement("p");
+    p.className = "innerParagraph";
     p.innerText = txt;
     mainDiv.appendChild(p);
-    tripsUI.appendChild(mainDiv);
-    if (!img && scrollLast == true)
-        tripsUI.lastChild.scrollIntoView({ behavior: 'smooth' });
-
-
+    travelUI.appendChild(mainDiv);
+    if (!img && scrollSmooth == true)
+        travelUI.lastChild.scrollIntoView({ behavior: "smooth" });
 }
 
-const displayNoTrips = async() => {
-    const tripsFromCache = await getTripsFromCache();
-    const noTripsSection = document.getElementById('noTrips')
-    console.log('no trips', noTripsSection);
-    noTripsSection.style.display = !tripsFromCache || tripsFromCache.length == 0 ? "block" : "none";
-}
+const noneTravels = async () => {
+    const tripsFromCache = await getLocalTravels();
+    const woTravelsSection = document.getElementById("woTravels");
+    woTravelsSection.style.display =
+        !tripsFromCache || tripsFromCache.length == 0 ? "block" : "none";
+};
 
-
-
-export { addTrip, dateChecker }
+export { setTravel };
